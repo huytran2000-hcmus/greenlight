9f1c2cd5f0ec0199413fc778f9c17b36f66b3ea4
Trần Minh Huy <minhhuy9a3dt@gmail.com>
2023-05-17 01:17:40 +0700

    Add delete a movie

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -99,6 +99,25 @@ func (m *MovieModel) Update(movie *Movie) error {
 	return nil
 }
 
-func (m *Movie) Delete(id int64) error {
+func (m *MovieModel) Delete(id int64) error {
+	stmt := `
+    DELETE FROM movies
+    WHERE id = $1
+    `
+
+	result, err := m.DB.Exec(stmt, id)
+	if err != nil {
+		return fmt.Errorf("data: delete a movie: %s", err)
+	}
+
+	rowsAffected, err := result.RowsAffected()
+	if err != nil {
+		return err
+	}
+
+	if rowsAffected == 0 {
+		return ErrRecordNotFound
+	}
+
 	return nil
 }
